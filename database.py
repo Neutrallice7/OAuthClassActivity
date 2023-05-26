55a14c15be2c2928d68e7a634fb3eab0108ab226
Sulthan Syuza Prabowo <sulthanprabowo7@gmail.com>
2023-05-26 19:34:14 +0700

    comments on database.py

--- database.py
+++ database.py
@@ -2,10 +2,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+# Location of database file
 DATABASE_URL = "sqlite:///./database.db"
 
+# create_engine creates a new engine instance that connects to the database specified in DATABASE_URL.
+# connect_args is used to make sure that SQLite works well with threads.
 engine = create_engine(DATABASE_URL, connect_args = {"check_same_thread": False})
 
+# sessionmaker creates a class SessionLocal that can be used to create a database session.
 SessionLocal = sessionmaker(autocommit = False, autoflush = False, bind = engine)
 
 Base = declarative_base()
