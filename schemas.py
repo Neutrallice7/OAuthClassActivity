c708acc103c8cba71d3903f96672b4ea67f00187
Carmen1104 <carmencongko11@gmail.com>
2023-05-26 13:51:20 +0700

    add sql

---
+++ schemas.py
@@ -0,0 +1,17 @@
+import datetime as _dt
+import pydantic as _pydantic
+
+class userBase(_pydantic.Basemodel):
+    email: str
+
+class userCreate(userBase):
+    hashed_password :str
+
+    class Config:
+        orm_mode = True
+
+class User(userBase):
+    id: int
+
+    class Config:
+        orm_mode = True
